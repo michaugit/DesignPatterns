6fb7b4714ec18340a22d8dbac2c30a13738b2815
michal_bitbucket <michal.pieniadz99@gmail.com>
2020-12-05 21:42:00 +0100

    php website delete

--- AndroidFileUpload/fileUpload.php
+++ AndroidFileUpload/fileUpload.php
@@ -18,11 +18,10 @@ if (isset($_FILES['image']['name'])) {
  
     // reading other post parameters
     $email = isset($_POST['email']) ? $_POST['email'] : '';
-    $website = isset($_POST['website']) ? $_POST['website'] : '';
  
     $response['file_name'] = basename($_FILES['image']['name']);
     $response['email'] = $email;
-    $response['website'] = $website;
+
  
     try {
         // Throws exception incase file is not being moved
